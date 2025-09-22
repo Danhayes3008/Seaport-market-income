e3e166de32dc3cde865ceebcbc8ce5b3715a7619
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2025-09-22 15:35:10 +0100

    initial commit of data
    started the array that will contain the data for this site

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var commodities = 
+[
+    {name: "cocoa", gold: 50, stone: 0, wood: 0, iron: 0, fish: 0},
+    {name: "wheat", gold: 0, stone: 0, wood: 0, iron: 0, fish: 60},
+    {name: "grape vine", gold: 0, stone: 0, wood: 45, iron: 0, fish: 0},
+    {name: "sugarcane", gold: 0, stone: 20, wood: 0, iron: 0, fish: 0},
+    {name: "wine", gold: 0, stone: 27, wood: 0, iron: 0, fish: 80},
+    {name: "flour", gold: 59, stone: 0, wood: 53, iron: 0, fish: 0},
+]
